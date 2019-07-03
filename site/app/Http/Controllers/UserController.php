70371558e5a3f05114efd0b420134eb7158ae280
tassanai <tassanai.s@mono.co.th>
2019-07-03 18:24:03 +0700

    เพิ่ม migration

--- site/app/Http/Controllers/UserController.php
+++ site/app/Http/Controllers/UserController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\User;
 
 class UserController extends Controller
 {
     public function login(Request $request) 
     {
+        dd(User::all());
+        // return User::all();
+        // var_dump($user);
+        // die();
         return [
             'api' => 'lumen 5.8',
             'company' => 'busble',
-            'username' => $request->input('username'),
-            'password' => $request->input('password')
+            // 'username' => $request->input('username'),
+            // 'password' => $request->input('password')
         ];
     }
     public function getUser(Request $request, $id)
---
+++ site/database/migrations/2019_07_03_102946_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('telno')->nullable(true)->unique();
+            $table->string('email')->nullable(true)->unique();
+            $table->string('gender')->nullable(true);
+            $table->string('thumbnail')->nullable(true);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
